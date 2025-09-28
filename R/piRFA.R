3d349e60c582c5f2fc405ea553168fd501f5bce4
Cesar Merino-Soto <sikayax@yahoo.com.ar>
2025-09-28 12:30:10 -0600

    ...

--- R/piRFA.R
+++ R/piRFA.R
@@ -98,20 +98,28 @@
 #' https://doi.org/10.1177/0013164411427395 (Original work published 2012)
 #'
 #' Oort, F. J. (1992). Using restricted factor analysis to detect item bias. *Psychological Methods*, 37, 547–567.
+#'
 #' Oort, F. J. (1998). Simulation study of item bias detection with restricted factor analysis.
 #' *Structural Equation Modeling*, 5, 107–124.
+#'
 #' French, B. F., & Finch, W. H. (2008). Multigroup confirmatory factor analysis: Locating the invariant referent variables.
 #' *Structural Equation Modeling*, 15(1), 96–113.
+#'
 #' Stark, S., Chernyshenko, O. S., & Drasgow, F. (2006). Detecting differential item functioning with confirmatory factor analysis and item response theory: Toward a unified strategy.
 #' *Journal of Applied Psychology*, 91(6), 1292–1306.
+#'
 #' Kolbe, L., & Jorgensen, T. D. (2018). Using product indicators in restricted factor analysis models to detect nonuniform measurement bias.
 #' In *Quantitative Psychology* (pp. 235–245). Springer.
+#'
 #' Kolbe, L., & Jorgensen, T. D. (2019). Using restricted factor analysis to select anchor items and detect differential item functioning.
 #' *Behavior Research Methods*, 51, 138–151.
+#'
 #' Kolbe, L., Jorgensen, T. D., & Molenaar, D. (2020). The Impact of Unmodeled Heteroskedasticity on Assessing Measurement Invariance in Single-group Models.
 #' *Structural Equation Modeling*, 28(1), 82–98.
+#'
 #' Whittaker, T. A. (2012). Estimation of Standardized Expected Parameter Change for DIF Detection.
 #' *Educational and Psychological Measurement*, 72(3), 342-357.
+#'
 #' Garnier-Villarreal, M., & Jorgensen, T. D. (2024). Evaluating Local Model Misspecification with Modification Indices in Bayesian Structural Equation Modeling.
 #' *Structural Equation Modeling*, 1–15.
 #'
@@ -133,7 +141,7 @@ piRFA <- function(data, items, cov, lvname = "LatFact", est = "MLM",
     any.out <- do.call(rbind, lapply(mimic.param, function(x)
       lavaan::lavTestScore(fit.mimic, add = x)$test))
 
-    sep.out <- lavaan::lavTestScore(fit.mimic, add = as.character(mimicparam))
+    sep.out <- lavaan::lavTestScore(fit.mimic, add = as.character(mimic.param))
 
     # Baseline chi2 and df
     baseline <- fit.mimic@test$standard
